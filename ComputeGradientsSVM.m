00e0c7befd3e0e9451f12a2748df20430b3428a6
Anton Lu <anton.lu@epfl.ch>
2019-04-02 14:45:28 +0200

    Add SVM loss cost

--- ComputeGradientsSVM.m
+++ ComputeGradientsSVM.m
@@ -1,9 +1,18 @@
-function [gradW, gradb] = ComputeGradientsSVM(X, Y, P, W, lambda)
+function [gradW, gradb] = ComputeGradientsSVM(X, Y, b, W, lambda)
 
 xSize = size(X);
 batchSize = xSize(2);
 
-Gbatch = -(Y - P);
+S = W * X + b * ones(1, size(X, 2));
+
+Sy = S .* Y;
+
+Gbatch = heaviside(S - sum(Sy) + 1);
+
+for i=1:batchSize
+    pos = find(Y(:, i) == 1);
+    Gbatch(pos, i) = -sum(Gbatch(:, i)) + Gbatch(pos, i);
+end
 
 dLdW = (1 / batchSize) * Gbatch * X';
 dLdb = (1 / batchSize) * Gbatch * ones(batchSize, 1);
