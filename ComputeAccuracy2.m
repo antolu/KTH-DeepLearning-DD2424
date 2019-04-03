01317738c27a18cee74faab4c5c0fe1ef6204f9d
Anton Lu <anton.lu@epfl.ch>
2019-04-03 13:06:22 +0200

    Split 2layer evaluation to other,accuracy matrix

--- ComputeAccuracy2.m
+++ ComputeAccuracy2.m
@@ -3,18 +3,12 @@ function acc = ComputeAccuracy2(X, y, W, b)
 xSize = size(X);
 numberOfSamples = xSize(2);
 
-H = EvaluateClassifier(X, W{1}, b{1}); H(H < 0) = 0;
-P = SoftMax(EvaluateClassifier(X, W{2}, b{2}));
+[P, H] = Evaluate2Layer(X, W, b);
 
-acc = 0;
+V = P==max(P);
+matches = V(sub2ind(size(V), y', 1:size(y)));
 
-for i=1:numberOfSamples
-    k = find(P(:, i) == max(P(:, i)));
-    
-    if k == y(i)
-        acc  = acc + 1;
-    end
-end
+acc = sum(matches);
 
 acc = acc / numberOfSamples;
 
